4e9bc7933010644c2038d2ed7181a044c619184a
Дмитрий Ершов <ershov_dmitriy@bk.ru>
2021-09-18 12:30:09 +0300

    Добавьте файлы проекта.

---
+++ BaseTest/BaseTest/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace BaseTest
+{
+    public partial class App : Application
+    {
+        static BaseStadium basestadium;
+
+        public static BaseStadium BaseStadium
+        {
+            get
+            {
+                if (basestadium == null)
+                {
+                    basestadium = new BaseStadium(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Stadiums.db3"));
+                }
+                return basestadium;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()   
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ BaseTest/BaseTest/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BaseTest
+{
+   
+    public partial class MainPage : ContentPage
+    {
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            StadiumView.ItemsSource = await App.BaseStadium.GetStadiumsAsync();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(LocationEntry.Text) && !string.IsNullOrWhiteSpace(SizeEntry.Text) && !string.IsNullOrWhiteSpace(DescriptionEntry.Text) && !string.IsNullOrWhiteSpace(NameEntry.Text))
+            {
+                await App.BaseStadium.SaveStadiumAsync(new Stadium
+                {
+                    Name = NameEntry.Text,
+                    Location = LocationEntry.Text,
+                    Size = int.Parse(SizeEntry.Text),
+                    Description = DescriptionEntry.Text
+                });
+
+                NameEntry.Text = SizeEntry.Text = LocationEntry.Text = DescriptionEntry.Text = string.Empty;
+                StadiumView.ItemsSource = await App.BaseStadium.GetStadiumsAsync();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
